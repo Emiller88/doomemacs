8a39c325c63fd5b3caa23fec7cfd412cef081cbf
Itai Y. Efrat <itai3397@gmail.com>
2021-04-28 23:07:22 +0300

    selectrum: add bibtex-actions to tools: biblio

--- modules/tools/biblio/packages.el
+++ modules/tools/biblio/packages.el
@@ -6,3 +6,5 @@
   (package! ivy-bibtex :pin "ca09076c3d6e51cc4ffe208c8176fccf3710fcc6"))
 (when (featurep! :completion helm)
   (package! helm-bibtex :pin "ca09076c3d6e51cc4ffe208c8176fccf3710fcc6"))
+(when (featurep! :completion selectrum)
+  (package! bibtex-actions :pin "743f548c0cd46e3418a7ca4736bde8c86f97c073"))
